363c3c125cd581780164220be99bc9fe60148617
agolebiowska <agataxgolebiowska@gmail.com>
2020-07-07 16:43:12 +0200

    init

---
+++ cmd/cdg/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func main() {
+	pixelgl.Run(run)
+}
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:                  "Companion driven Game",
+		Bounds:                 pixel.R(0, 0, 1024, 768),
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	if !win.Closed() {
+		win.Update()
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module cdg
+
+go 1.13
+
+require github.com/faiface/pixel v0.9.0
